7ac841df3163aea6e2ae1b9cd8a74beb7be6ad0c
Roshan B <63301253+DdarkLord69@users.noreply.github.com>
2021-01-06 16:03:02 +0530

    Feat(init): Add boiler plate code

---
+++ src/main/java/edu/nitt/delta/meettracker/MeetTrackerApplication.java
@@ -0,0 +1,13 @@
+package edu.nitt.delta.meettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nitt/delta/meettracker/MeettrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nitt.delta.meettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeettrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
